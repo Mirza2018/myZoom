64c644c20c7e4b086043139dc8560a42a364509c
mirza2018 <mirza.eee.4th@gmail.com>
2026-01-12 07:37:42 +0600

    socket join-call error fixed

--- src/controllers/socketManager.js
+++ src/controllers/socketManager.js
@@ -14,6 +14,8 @@ export const connectToSocket = (server) => {
   });
 
   io.on("connection", (socket) => {
+    console.log("SOCKET CONNECTED");
+
     socket.on("join-call", (path) => {
       if (connections[path] === undefined) {
         connections[path] = [];
@@ -22,21 +24,32 @@ export const connectToSocket = (server) => {
       timeOnline[socket.id] = new Date();
 
       // connections[path].forEatch(elem => {
-      //   io.to(elem)
+      //   io.to(elem)s
       // })
       for (let a = 0; a < connections[path].length; a++) {
         io.to(connections[path][a]).emit("user-joined", socket.id);
       }
+
+      // if (messages[path] !== undefined) {
+      //   for (let a = 0; a < messages[path].length; ++a) {
+      //     io.to(socket.id).emit(
+      //       "chat-message",
+      //       messages[path][a]["socket-id-sender"]
+      //     );
+      //   }
+      // }
+      if (messages[path] !== undefined) {
+        for (let a = 0; a < messages[path].length; ++a) {
+          io.to(socket.id).emit(
+            "chat-message",
+            messages[path][a]["data"],
+            messages[path][a]["sender"],
+            messages[path][a]["socket-id-sender"]
+          );
+        }
+      }
     });
 
-    if (messages[path] === undefined) {
-      for (let a = 0; a < messages[path].length; ++a) {
-        io.to(socket.id).emit(
-          "chat-message",
-          messages[path][a]["socket-id-sender"]
-        );
-      }
-    }
     socket.on("signal", (toId, message) => {
       io.to(toId).emit("signal", socket.id, message);
     });
@@ -93,5 +106,5 @@ export const connectToSocket = (server) => {
     });
   });
 
-  return io;
+  return io ;
 };
